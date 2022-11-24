0dc4a3f86743606a96f095eb9143726f5fc4b33d
romulo <romulojnr@gmail.com>
2022-11-24 09:52:25 -0300

    feat: visual animation

--- src/aulapolimorfismo/SimuladorAnimal.java
+++ src/aulapolimorfismo/SimuladorAnimal.java
@@ -8,19 +8,23 @@ public class SimuladorAnimal {
             System.out.println(animal.getClass() +" - "+ animal.getX());
         }
     }
-    public static void main(String args[]){
+    public static void main(String args[]) throws InterruptedException{
     	
     	Animal animais[] = new Animal[3];
     	animais[0] = new Anfibio(0.,0.);
         animais[1] = new Ave(0.,10.);
         animais[2] = new Peixe(0.,20.);
          
+        TelaSimulacao tela = new TelaSimulacao(animais);
+        tela.inicializar();
+        
         SimuladorAnimal simulador = new SimuladorAnimal();
         
         for (int i = 0; i < 10; i++) {
 			simulador.executarSimulacao(animais);
+			Thread.sleep(100);
+			tela.repaint();
 		}
-        TelaSimulacao tela = new TelaSimulacao(animais);
-        tela.inicializar();
+        
     }
 }
